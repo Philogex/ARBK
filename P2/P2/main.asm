132003d032880c63b21dbe3ea1432c0dbb0df768
Philogex <64011915+Philogex@users.noreply.github.com>
2023-10-20 16:56:00 +0200

    changed output to PB0 and PB1 added Pullup for Input

--- P2/P2/main.asm
+++ P2/P2/main.asm
@@ -3,9 +3,12 @@
 ;
 ; Created: 19.10.2023 17:15:13
 ; Author : gx
-; Description: Uses PD0 and PD1 to control D0 and D9 respectively, as well as PD2 and PD3 as interrupt inputs connected to SW1 and SW2 respectively
+; Description: Uses PB0 and PB1 to control D0 and D9 respectively, as well as PD2 and PD3 as interrupt inputs connected to SW1 and SW2 respectively
+;
 
-.include <m328pdef.inc>
+.include "m328pdef.inc"
+
+.cseg
 
 .def led_state_d0d9 = r17 ; 4bit d0-logic + 4bit d9-logic ; Reset idx3 (should never be set), Blink idx2, On idx1, Off idx0
 .def led_output = r18
@@ -16,8 +19,6 @@
 .equ F_CPU = 16000000
 .equ Prescaler = 1024
 .equ DelayCycles = 10; (F_CPU / Prescaler) / 5
-
-.cseg
 
 .org 0x0000
     rjmp init
@@ -34,33 +35,34 @@ init:
     ldi r16, high(RAMEND)
     out SPH, r16
 
-    ldi r16, (1 << DDD0) | (1 << DDD1) ; 0b00000011
-    out DDRD, r16 ; D0 / D9 LED output
+	ldi r16, (0 << DDD2) | (0 << DDD3) ; 0b00000000
+	out DDRD, r16 ; PD2 / PD3 for SW1 / SW2 respectively
 
-    ldi r16, (0 << DDD2) | (0 << DDD3) ; 0b00000000
-    out PORTD, r16 ; set D0 / D9 to off
-    out DDRD, r16 ; INT0 / INT1 for SW1 / SW2 respectively
+	ldi r16, 0b11111111
+    out PORTD, r16 ; pullup for Input
+
+    ldi r16, (1 << DDB0) | (1 << DDB1) ; 0b00000011
+	out DDRB, r16 ; PB0 / PB1 for D0 / D9 LED output respectively 
+
+	ldi d0_inverter, (1 << 5) | (1 << 4)
+    ldi d9_inverter, (1 << 1) | (1 << 0)
+
+	ldi led_state_d0d9, (1 << 4) | (1 << 0)
 
     ldi r16, (1 << WGM12) | (1 << CS10) | (1 << CS12) ; CTC and 1024 Prescaler
     sts TCCR1B, r16
 
-    ldi r16, (1 << INT1) | (1 << INT0)
-    out EIMSK, r16 ; enable external interrupt source INT0 and INT1
-    ; EIFR (1 << INTF1) | (1 << INTF0) can be set now
-
-    ldi r16, (1 << ISC01) | (1 << ISC11)
+	ldi r16, (1 << ISC01) | (1 << ISC11)
     sts EICRA, r16
+
+    ldi r16, (1 << INT1) | (1 << INT0)
+    out EIMSK, r16 ; enable external interrupt source INT0 and INT1 [EIFR (1 << INTF1) | (1 << INTF0) can be set now]
 
     ldi r16, low(DelayCycles)
     sts OCR1AL, r16
 
     ldi r16, high(DelayCycles)
     sts OCR1AH, r16
-
-    ldi d0_inverter, (1 << 5) | (1 << 4)
-    ldi d9_inverter, (1 << 1) | (1 << 0)
-
-	ldi led_state_d0d9, (1 << 4) | (1 << 0)
 
     ; enable interrupts
     sei
@@ -76,17 +78,17 @@ main_loop:
 
     ; d0_off
     sbrc led_state_d0d9, 4
-    ldi led_output, (0 << DDD0)
+    ldi led_output, (0 << DDB0)
     ; d9_off
     sbrc led_state_d0d9, 0
-    ldi led_output, (0 << DDD1)
+    ldi led_output, (0 << DDB1)
 
     ; d0_on
     sbrc led_state_d0d9, 5
-    ldi led_output, (1 << DDD0)
+    ldi led_output, (1 << DDB0)
     ; d9_on
     sbrc led_state_d0d9, 1
-    ldi led_output, (1 << DDD1)
+    ldi led_output, (1 << DDB1)
 
     ; d0_blink
     sbrc led_state_d0d9, 6
@@ -95,15 +97,15 @@ main_loop:
     sbrc led_state_d0d9, 2
     eor led_state_d0d9, d9_inverter
 
-    out PORTD, led_output
+    out PORTB, led_output
 
     rjmp main_loop
 
 int0_handler:
     ; rising edge of PD2 / SW1
     ; make sure, that other led is off
-    sbrs led_state_d0d9, 0
-    reti
+    ; sbrs led_state_d0d9, 0
+    ; reti
 
     ; off -> on
     sbrc led_state_d0d9, 4
@@ -122,8 +124,8 @@ int0_handler:
 int1_handler:
     ; rising edge of PD3 / SW2
     ; make sure, that other led is off
-    sbrs led_state_d0d9, 4
-    reti
+    ; sbrs led_state_d0d9, 4
+    ; reti
 
     ; off -> on
     sbrc led_state_d0d9, 0
